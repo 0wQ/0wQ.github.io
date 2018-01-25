8130218a5f5713289a909c6f43d2e93c1cf20d5b
wsly47 <me@mizore.cn>
2018-01-26 06:33:13 +0800

    blablabla

--- src/load.js
+++ src/load.js
@@ -3,7 +3,7 @@ getScript('/src/headroom.min.js', () => {
     const elem = document.querySelector('header');
     new Headroom(elem, {
       tolerance: 20,
-      offset: 200,
+      offset: 100,
       classes: {
         initial: 'animated',
         pinned: 'slideDown',
