7ff04953794a340e462d51b316859e0d32e34821
Mizore <me@mizore.cn>
2018-09-13 07:13:58 +0800

    Update

--- src/load.js
+++ src/load.js
@@ -57,7 +57,7 @@ function getScript(a, b) {
 }
 function getValine(path) {
   getScript('//cdn.jsdelivr.net/npm/leancloud-storage/dist/av-min.js', function () {
-    getScript('//cdn.jsdelivr.net/npm/valine@1.3.0/dist/Valine.min.js', function () {
+    getScript('//cdn.jsdelivr.net/npm/valine@1.3.1/dist/Valine.min.js', function () {
       new Valine({
         el: '#comment',
         lang: 'en',
